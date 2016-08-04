da2a15a9525aef38da775d03e81e524f890ae875
PatrickPriestley <patrickp@q4websystems.com>
2016-08-04 14:05:26 -0400

    Adding class to testrunner

--- src/test/java/testrunner/SmokeTest.java
+++ src/test/java/testrunner/SmokeTest.java
@@ -2,10 +2,11 @@ package testrunner;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import specs.Dashboard.DashboardSearch;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-
+        DashboardSearch.class
 })
 
 public class SmokeTest {
