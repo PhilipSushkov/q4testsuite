7fae9b10804ab5fe3995ec72c3379b25c35b21b9
PatrickPriestley <patrickp@q4websystems.com>
2016-08-05 11:05:23 -0400

    Removing repo and token from trigger
    These are passed in as params

--- cron/trigger_nightly_build.sh
+++ cron/trigger_nightly_build.sh
@@ -6,7 +6,7 @@ _circle_token=$3
 _environment=$4
 _suite_name=$5
 
-trigger_build_url=https://circleci.com/api/v1/project/PatrickPriestley/q4testsuite/tree/master?circle-token=5439aa08dd79b73f2f13535f252c0d69dbb31edb
+trigger_build_url=https://circleci.com/api/v1/project/${_project}/tree/${_branch}?circle-token=${_circle_token}
 
 post_data=$(cat <<EOF
 {
