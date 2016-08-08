f201c2627c214d40bbf0b0f4a4c66d25f56f6e94
philip_sushkov <philips@q4websystems.com>
2016-08-08 09:01:36 -0400

    Aug 08, 2016, ActivityPage

--- src/test/java/pageobjects/activityPage/ActivityPage.java
+++ src/test/java/pageobjects/activityPage/ActivityPage.java
@@ -3,11 +3,12 @@ package pageobjects.activityPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import pageobjects.AbstractPageObject;
+import pageobjects.Page;
 
 /**
  * Created by philipsushkov on 2016-08-07.
  */
-public class ActivityPage extends AbstractPageObject {
+public class ActivityPage extends Page {
 
     private final By pageTitle = By.cssSelector(".note-manager-header .page-title");
 
