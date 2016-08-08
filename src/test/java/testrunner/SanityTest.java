d6a94e9dcc919933a8b025732ba1c9b1e3b0f389
PatrickPriestley <patrickp@q4websystems.com>
2016-08-08 17:07:50 -0400

    Removed unused imports and reverted breaking change

--- src/test/java/testrunner/SanityTest.java
+++ src/test/java/testrunner/SanityTest.java
@@ -2,11 +2,8 @@ package testrunner;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import specs.dashboard.DashboardLinksToMyCompany;
-import specs.dashboard.DashboardSearch;
 import specs.loginPage.ForgotPassword;
 import specs.loginPage.UserLogin;
-import specs.sideNavBar.TabNavigationExpanded;
 
 /**
  * Created by patrickp on 2016-08-08.
