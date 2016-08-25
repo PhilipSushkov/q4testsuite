829fa193d1898f86a40c5c59fc602138c0c2f3a8
PatrickPriestley <patrickp@q4websystems.com>
2016-08-25 13:50:51 -0400

    Adding tests to test runner

--- src/test/java/testrunner/NightRun.java
+++ src/test/java/testrunner/NightRun.java
@@ -2,6 +2,9 @@ package testrunner;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import specs.activity.LogActivityFromActivityPage;
+import specs.activity.SearchForActivity;
+import specs.contacts.AddRemoveContacts;
 import specs.dashboard.DashboardBuildReport;
 import specs.dashboard.DashboardLogActivity;
 import specs.dashboard.DashboardLinksToMyCompany;
@@ -9,6 +12,7 @@ import specs.dashboard.DashboardSearch;
 import specs.loginPage.ForgotPassword;
 import specs.loginPage.UserLogin;
 import specs.sideNavBar.TabNavigationExpanded;
+import specs.watchlist.EditWatchlist;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
@@ -18,7 +22,11 @@ import specs.sideNavBar.TabNavigationExpanded;
         ForgotPassword.class,
         DashboardLinksToMyCompany.class,
         DashboardLogActivity.class,
-        DashboardBuildReport.class
+        DashboardBuildReport.class,
+        SearchForActivity.class,
+        LogActivityFromActivityPage.class,
+        AddRemoveContacts.class,
+        EditWatchlist.class
 })
 
 public class NightRun {
