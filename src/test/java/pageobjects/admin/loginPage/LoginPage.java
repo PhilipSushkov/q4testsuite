a41b501728cb719ad2628b60d743f271f37f1604
PatrickPriestley <patrickp@q4websystems.com>
2016-09-20 11:45:15 -0400

    Starting to write admin tests
    New admin account created. Google login automated

--- src/test/java/pageobjects/admin/loginPage/LoginPage.java
+++ src/test/java/pageobjects/admin/loginPage/LoginPage.java
@@ -10,6 +10,10 @@ import pageobjects.AbstractPageObject;
 public class LoginPage extends AbstractPageObject {
     private final By loginButton = By.cssSelector("#login-page > div > button.login-button");
     private final By googleLoginButton = By.cssSelector("#a0-onestep > div.a0-mode-container > div > form > div.a0-body-content > div.a0-collapse-social > div.a0-iconlist > div > span");
+    private final By enterEmail = By.id("Email");
+    private final By enterPassword = By.id("Passwd");
+    private final By submit = By.id("signIn");
+    private final By nextButton = By.id("next");
 
     public LoginPage(WebDriver driver) {
         super(driver);
@@ -18,6 +22,11 @@ public class LoginPage extends AbstractPageObject {
     public LoginPage loginAdmin() {
         findElement(loginButton).click();
         findElement(googleLoginButton).click();
+        switchToNewTab();
+        findElement(enterEmail).sendKeys("test@q4websystems.com");
+        findElement(nextButton).click();
+        findElement(enterPassword).sendKeys("testing!");
+        findElement(submit).click();
 
         return this;
     }
