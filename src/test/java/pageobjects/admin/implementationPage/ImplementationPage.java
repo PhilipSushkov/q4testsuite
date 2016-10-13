6362d304905860ffd9a849bfd25d8aa5503e663f
PatrickPriestley <patrickp@q4websystems.com>
2016-10-13 08:51:29 -0400

    Adding more implementations tests

--- src/test/java/pageobjects/admin/implementationPage/ImplementationPage.java
+++ src/test/java/pageobjects/admin/implementationPage/ImplementationPage.java
@@ -16,6 +16,9 @@ public class ImplementationPage extends AbstractPageObject {
     private final By sourceField = By.name("sourceURL");
     private final By saveButton = By.cssSelector("body > q4-app > div > div > q4-implementation > div.modal > p-dialog > div > div.ui-dialog-content.ui-widget-content > q4-implementation-create > div > button.button.button-yellow");
     private final By implementationList = By.cssSelector(".ui-datatable table");
+    private final By firstImplementation = By.cssSelector("body > q4-app > div > div > q4-implementation > p-datatable > div > div > table > tbody > tr:nth-child(1) > td:nth-child(3) > span > div > button");
+    private final By errorMessage = By.cssSelector("body > q4-app > div > div > q4-implementation > div.modal > p-dialog > div > div.ui-dialog-content.ui-widget-content > q4-implementation-create > div.error");
+    private final By cancelButton = By.cssSelector("body > q4-app > div > div > q4-implementation > div.modal > p-dialog > div > div.ui-dialog-content.ui-widget-content > q4-implementation-create > div > button.button.button-no-background");
 
     public ImplementationPage(WebDriver driver) {
         super(driver);
@@ -34,5 +37,40 @@ public class ImplementationPage extends AbstractPageObject {
     public String getImplementationList() {
         wait.until(ExpectedConditions.invisibilityOfElementLocated(loading));
         return findElement(implementationList).getText();
+    }
+
+    public ImplementationPage selectFirstImplementation() {
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(loading));
+        findElement(firstImplementation).click();
+
+        return this;
+    }
+
+    public ImplementationPage editDetails(String sourceUrl) {
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(loading));
+        findElement(sourceField).clear();
+        findElement(sourceField).sendKeys(sourceUrl);
+        findElement(saveButton).click();
+
+        return this;
+    }
+
+    public String getError() {
+        return findElement(errorMessage).getText();
+    }
+
+    public ImplementationPage openNewImplementationModal() {
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(loading));
+        findElement(addButton).click();
+
+        return this;
+    }
+
+
+    public ImplementationPage cancelImplementationCreation() {
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(loading));
+        findElement(cancelButton).click();
+
+        return this;
     }
 }
--- src/test/java/specs/admin/implementation/AddEditImplementations.java
+++ src/test/java/specs/admin/implementation/AddEditImplementations.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang.RandomStringUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import pageobjects.admin.implementationPage.ImplementationPage;
 import pageobjects.admin.loginPage.LoginPage;
 import specs.AdminAbstractSpec;
@@ -29,5 +30,42 @@ public class AddEditImplementations extends AdminAbstractSpec {
         ImplementationPage implementationPage = new ImplementationPage(driver).addNewImplementation(alias, sourceUrl);
 
         Assert.assertThat(implementationPage.getImplementationList(), containsString(alias));
+    }
+
+    @Test
+    public void canEditImplementation() {
+        String sourceUrl = "www." + RandomStringUtils.randomAlphanumeric(5) + ".com";
+
+        ImplementationPage implementationPage = new ImplementationPage(driver).selectFirstImplementation()
+                .editDetails(sourceUrl);
+
+        Assert.assertThat(implementationPage.getImplementationList(), containsString(sourceUrl));
+    }
+
+    @Test
+    public void cannotAddImplementationWithoutSourceUrl() {
+        String alias = "Alias" + RandomStringUtils.randomAlphanumeric(5);
+        String emptyUrl = "";
+        ImplementationPage implementationPage = new ImplementationPage(driver).addNewImplementation(alias, emptyUrl);
+
+        Assert.assertThat(implementationPage.getError(), containsString("ClientProxy validation failed"));
+    }
+
+    @Test
+    public void cannotAddImplementationWithoutAlias() {
+        String emptyAlias = "";
+        String url = "www.test.com";
+        ImplementationPage implementationPage = new ImplementationPage(driver).addNewImplementation(emptyAlias, url);
+
+        Assert.assertThat(implementationPage.getError(), containsString("ClientProxy validation failed"));
+    }
+
+    @Test
+    public void canCancelImplementationCreation() {
+        ImplementationPage implementationPage = new ImplementationPage(driver).openNewImplementationModal()
+                .cancelImplementationCreation();
+
+        // Assert that modal has been dismissed.
+        Assert.assertEquals(1, driver.findElements(By.className("ui-dialog-title")).size());
     }
 }
