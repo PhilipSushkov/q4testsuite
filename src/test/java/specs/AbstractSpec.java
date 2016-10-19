ed9692ec8a39632a8edac25f38a54ac15c0df8b9
PatrickPriestley <patrickp@q4websystems.com>
2016-10-19 14:56:11 -0400

    Adding space to message

--- src/test/java/specs/AbstractSpec.java
+++ src/test/java/specs/AbstractSpec.java
@@ -108,7 +108,7 @@ public abstract class AbstractSpec {
 
         if (getActiveEnvironment() != EnvironmentType.LOCAL) {
             driver.quit();
-            System.out.println(testMethodName + "complete");
+            System.out.println(testMethodName + " - " + "complete");
         }
     }
 
