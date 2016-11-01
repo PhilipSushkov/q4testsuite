d956102689ced44a0bdb2741255db89eda14e342
KelTez <macabessa123@gmail.com>
2016-11-01 10:51:56 -0400

    Created src/test/java/pageobjects/user/webAnalyticsPage/WebAnalyticsPage.java path
    Not much new except for the addition of a layout for the Web Analytics
    page elements, to be done at a later stage.

--- src/test/java/pageobjects/user/webAnalyticsPage/WebAnalyticsPage.java
+++ src/test/java/pageobjects/user/webAnalyticsPage/WebAnalyticsPage.java
@@ -6,7 +6,74 @@ import pageobjects.AbstractPageObject;
 /**
  * Created by patrickp on 2016-08-16.
  */
+
 public class WebAnalyticsPage extends AbstractPageObject{
+
+
+    /**     HEADER       */
+
+    //buttons
+
+        //private final By dayBtn
+        //private final By monthBtn
+        //private final By yearBtn
+
+    //icons
+
+        //private final By sessionsIcon_
+        //private final By newSessionsIcon
+        //private final By usersIcon
+        //private final By newUsersIcon
+        //private final By pages_SessionsIcon
+        //private final By bounceRateIcon
+
+    //data
+
+        //private final By sessionsData_
+        //private final By newSessionsData
+        //private final By usersData
+        //private final By newUsersData
+        //private final By pages_SessionsData
+        //private final By bounceRateData
+        //private final By dateRange
+
+
+    /**     TRAFFIC_IMPACT     */
+
+    //buttons
+
+        //private final By sessionsBtn
+        //private final By news_EventsBtn
+
+    //data
+
+
+
+    /**     DEVICE_TYPES     */
+
+    //icons
+
+        //private final By desktopIcon
+        //private final By mobileIcon
+        //private final By tabletIcon
+
+    //data
+
+        //private final By desktopData
+        //private final By mobileData
+        //private final By tabletData
+
+
+    /**     BREAKDOWN_CHARTS     */
+
+
+    /**     BREAKDOWN_TABLES     */
+
+
+    /**     LOCATIONS        */
+
+
+
     public WebAnalyticsPage(WebDriver driver) {
         super(driver);
     }
---
+++ src/test/java/specs/user/WebAnalytics/WebAnalytics.java
@@ -0,0 +1,12 @@
+package specs.user.WebAnalytics;
+
+/**
+ * Created by kelvint on 11/1/16.
+ */
+public class WebAnalytics {
+
+
+
+
+
+}
