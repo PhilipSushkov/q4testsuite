7ab79f5c623d272f3df3ba4f94f5be84734e36ed
PatrickPriestley <patrickp@q4websystems.com>
2016-11-09 14:13:06 -0500

    Adding admin intelligence test classes
    Intelligence list, report details page

---
+++ src/test/java/pageobjects/admin/intelligencePage/ReportDetailsPage.java
@@ -0,0 +1,13 @@
+package pageobjects.admin.intelligencePage;
+
+import org.openqa.selenium.WebDriver;
+import pageobjects.AbstractPageObject;
+
+/**
+ * Created by patrickp on 2016-11-09.
+ */
+public class ReportDetailsPage extends AbstractPageObject {
+    public ReportDetailsPage(WebDriver driver) {
+        super(driver);
+    }
+}
---
+++ src/test/java/specs/admin/intelligence/IntelligenceList.java
@@ -0,0 +1,9 @@
+package specs.admin.intelligence;
+
+import specs.AbstractSpec;
+
+/**
+ * Created by patrickp on 2016-11-09.
+ */
+public class IntelligenceList extends AbstractSpec {
+}
