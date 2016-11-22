c2d76757d53de9155461ff40a75026d362223020
noelc <noelc@q4inc.com>
2016-11-22 13:41:11 -0500

    added admin tests for checking user roles

---
+++ src/test/java/pageobjects/admin/usersPage/UsersPage.java
@@ -0,0 +1,65 @@
+package pageobjects.admin.usersPage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import pageobjects.AbstractPageObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by noelc on 2016-11-22.
+ */
+public class UsersPage extends AbstractPageObject {
+
+    public UsersPage(WebDriver driver) {
+        super(driver);
+    }
+
+    private final By userRoleFilter = By.className("ui-dropdown-trigger");
+    private final By userRoleFilterList=By.xpath("//div[contains(@class,'ui-dropdown-panel')]//ul[contains(@class,'ui-dropdown-items')]");
+    private final By tableData=By.xpath("//tbody/tr/td[not(contains(@class,'dropdown-overflow'))]");
+
+    private ArrayList<WebElement> getUsers (){
+        waitForLoadingScreen();
+        ArrayList<WebElement> userRoles = new ArrayList<>(findElements(tableData));
+        return userRoles;
+    }
+
+    public UsersPage clickUserRoleFilter(){
+        waitForLoadingScreen();
+        wait.until(ExpectedConditions.elementToBeClickable(userRoleFilter));
+        findElement(userRoleFilter).click();
+        return this;
+    }
+
+    public UsersPage clickRoleInFilter (UserRole role){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(userRoleFilterList));
+        WebElement roleList=findElement(userRoleFilterList);
+        roleList.findElement(By.xpath("./li[span[contains(text(),'"+role.getRole()+"')]]")).click();
+        waitForLoadingScreen();
+        return this;
+    }
+
+    public boolean areUsersFilteredByRole(UserRole role){
+
+        ArrayList<WebElement> filteredUsers = getUsers();
+        boolean hasRole = false;
+        for (WebElement currentuser : filteredUsers){
+            ArrayList<WebElement> usersroles = new ArrayList<>(currentuser.findElements(By.tagName("button")));
+            for( WebElement userrole : usersroles){
+                if(userrole.getText().equals(role.getRole())){
+                    hasRole=true;
+                }
+            }
+            if(!hasRole){
+                return false;
+            }
+            else{
+                hasRole=false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/test/java/specs/admin/users/UserRoles.java
@@ -0,0 +1,79 @@
+package specs.admin.users;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import pageobjects.admin.loginPage.AdminLoginPage;
+import pageobjects.admin.usersPage.UserRole;
+import pageobjects.admin.usersPage.UsersPage;
+import specs.AdminAbstractSpec;
+
+/**
+ * Created by noelc on 2016-11-22.
+ */
+public class UserRoles extends AdminAbstractSpec{
+    @Before
+    public void setUp() {
+        new AdminLoginPage(driver).loginAdmin().navigateToUsersPage();
+
+    }
+
+
+    @Test @Ignore
+    public void filterAllUsers(){
+        UsersPage userPage = new UsersPage(driver);
+        userPage.clickUserRoleFilter().clickRoleInFilter(UserRole.ALL);
+        userPage.areUsersFilteredByRole(UserRole.ALL);
+
+    }
+
+    @Test
+    public void filterUsersByAdminRole(){
+        UsersPage userPage = new UsersPage(driver);
+        userPage.clickUserRoleFilter().clickRoleInFilter(UserRole.ADMIN);
+        Assert.assertTrue("Users were not filtered by the Admin role",userPage.areUsersFilteredByRole(UserRole.ADMIN));
+    }
+
+    @Test
+    public void filterUsersByUserRole(){
+        UsersPage userPage = new UsersPage(driver);
+        userPage.clickUserRoleFilter().clickRoleInFilter(UserRole.USER);
+        Assert.assertTrue("Users were not filtered by the user role",userPage.areUsersFilteredByRole(UserRole.USER));
+    }
+
+    @Test
+    public void filterUserByDeveloperRole(){
+        UsersPage userPage = new UsersPage(driver);
+        userPage.clickUserRoleFilter().clickRoleInFilter(UserRole.DEVELOPER);
+        Assert.assertTrue("Users were not filtered by the developer role",userPage.areUsersFilteredByRole(UserRole.DEVELOPER));
+    }
+
+    @Test
+    public void filterUserByIntelligenceRole(){
+        UsersPage userPage = new UsersPage(driver);
+        userPage.clickUserRoleFilter().clickRoleInFilter(UserRole.INTELLIGENCE_ANALYST);
+        Assert.assertTrue("Users were not filtered by the Intelligence role",userPage.areUsersFilteredByRole(UserRole.INTELLIGENCE_ANALYST));
+    }
+
+    @Test
+    public void filterUserByImplementationRole(){
+        UsersPage userPage = new UsersPage(driver);
+        userPage.clickUserRoleFilter().clickRoleInFilter(UserRole.IMPLEMENTATION_MANAGER);
+        Assert.assertTrue("Users were not filtered by the Implementation role",userPage.areUsersFilteredByRole(UserRole.IMPLEMENTATION_MANAGER));
+    }
+
+    @Test
+    public void filterUserByCustomerSuccessRole(){
+        UsersPage userPage = new UsersPage(driver);
+        userPage.clickUserRoleFilter().clickRoleInFilter(UserRole.CUSTOMER_SUCCESS_MANAGER);
+        Assert.assertTrue("Users were not filtered by the Customer Success role",userPage.areUsersFilteredByRole(UserRole.CUSTOMER_SUCCESS_MANAGER));
+    }
+
+    @Test
+    public void filterUserBySalesManagerRole(){
+        UsersPage userPage = new UsersPage(driver);
+        userPage.clickUserRoleFilter().clickRoleInFilter(UserRole.SALES_MANAGER);
+        Assert.assertTrue("Users were not filtered by the Sales Manager role",userPage.areUsersFilteredByRole(UserRole.SALES_MANAGER));
+    }
+}
