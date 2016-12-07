685c8a28fc5839ad16b6cd1fc707e68bf31805d7
noelc <noelc@q4inc.com>
2016-12-07 16:26:50 -0500

    parallel

--- src/test/java/testrunner/NightRun.java
+++ src/test/java/testrunner/NightRun.java
@@ -2,15 +2,6 @@ package testrunner;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import specs.user.dashboard.DashboardSearch;
-import specs.user.login.ForgotPassword;
-import specs.user.login.UserLogin;
-import specs.user.research.ResearchList;
-import specs.user.securityDetails.Overview;
-import specs.user.sideNavBar.TabNavigationExpanded;
-import specs.user.targeting.TargetingList;
-import specs.user.team.Q4TeamPage;
-import specs.user.watchlist.EditWatchlist;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
--- src/test/java/testrunner/SanityTest.java
+++ src/test/java/testrunner/SanityTest.java
@@ -2,13 +2,7 @@ package testrunner;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import specs.user.activity.FilterActivity;
-import specs.user.activity.SearchForActivity;
-import specs.user.contacts.ContactList;
-import specs.user.login.ForgotPassword;
-import specs.user.login.UserLogin;
 import specs.user.targeting.TargetingList;
-import specs.user.watchlist.EditWatchlist;
 
 /**
  * Created by patrickp on 2016-08-08.
