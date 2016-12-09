f353fdccf1adf8e4eee8cf55e882c521dec94413
noelc <noelc@q4inc.com>
2016-12-09 12:11:16 -0500

    adding in a thread variable "THREADS" which can be added to the curl command to set the thread count for the run.

--- src/test/java/testrunner/NightRun.java
+++ src/test/java/testrunner/NightRun.java
@@ -2,26 +2,43 @@ package testrunner;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import specs.user.activity.FilterActivity;
+import specs.user.activity.LogActivity;
+import specs.user.activity.SearchForActivity;
+import specs.user.contacts.ContactDetails;
+import specs.user.contacts.ContactList;
+import specs.user.dashboard.DashboardBuildReport;
+import specs.user.dashboard.DashboardLinks;
+import specs.user.dashboard.DashboardLogActivity;
+import specs.user.dashboard.DashboardSearch;
+import specs.user.login.ForgotPassword;
+import specs.user.login.UserLogin;
+import specs.user.research.ResearchList;
+import specs.user.securityDetails.Overview;
+import specs.user.sideNavBar.TabNavigationExpanded;
+import specs.user.targeting.TargetingList;
+import specs.user.team.Q4TeamPage;
+import specs.user.watchlist.EditWatchlist;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-
-        SuiteA.class,
-        SuiteB.class,
-        SuiteC.class,
-        SuiteD.class,
-        SuiteE.class
-       /*
-        Run two
+        LogActivity.class,
+        FilterActivity.class,
+        ContactList.class,
+        ContactDetails.class,
+        DashboardBuildReport.class,
+        DashboardLinks.class,
+        DashboardLogActivity.class,
         DashboardSearch.class,
         ForgotPassword.class,
         UserLogin.class,
+        Overview.class,
         TabNavigationExpanded.class,
         EditWatchlist.class,
         Q4TeamPage.class,
+        SearchForActivity.class,
         TargetingList.class,
-        Overview.class,
-        ResearchList.class*/
+        ResearchList.class
 })
 
 public class NightRun {
