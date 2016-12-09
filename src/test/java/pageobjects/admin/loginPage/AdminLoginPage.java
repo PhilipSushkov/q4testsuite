9ce4f983a609d88de89c07e7e5ece555a65b6b37
noelc <noelc@q4inc.com>
2016-12-09 14:32:30 -0500

    created a method where you enter the expected title of a window you are going to be opening. Will wait for that window and return true else false.

--- src/test/java/pageobjects/admin/loginPage/AdminLoginPage.java
+++ src/test/java/pageobjects/admin/loginPage/AdminLoginPage.java
@@ -57,6 +57,7 @@ public class AdminLoginPage extends AbstractPageObject {
         findElement(googleLoginButton).click();
 
         // Switch to new window opened
+        windowDidLoad("Sign in - Google Accounts");
         for(String winHandle : driver.getWindowHandles()){
             driver.switchTo().window(winHandle);
         }
@@ -73,4 +74,5 @@ public class AdminLoginPage extends AbstractPageObject {
 
         return this;
     }
+
 }
