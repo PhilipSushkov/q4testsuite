11b826f505758a7906ccfbcc07174451f1a01f7e
noelc <noelc@q4inc.com>
2017-01-05 13:22:15 -0500

    updated contact list checks to load entire list of contacts before grabbing it's contents

--- src/test/java/pageobjects/user/contactPage/ContactPage.java
+++ src/test/java/pageobjects/user/contactPage/ContactPage.java
@@ -2,6 +2,7 @@ package pageobjects.user.contactPage;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import pageobjects.AbstractPageObject;
 
 /**
@@ -11,14 +12,23 @@ public class ContactPage extends AbstractPageObject {
 
     private final By contactList = By.cssSelector(".contact-favorite-list");
     private final By firstContactInList = By.cssSelector(".contact-favorite-list-container .contact-favorite-list .x-dataview-item .column:nth-child(2)");
-
+    private final By showMoreLink = By.cssSelector(".load-more .x-button");
     public ContactPage(WebDriver driver) {
         super(driver);
     }
 
     public String getContacts() {
         waitForLoadingScreen();
+        loadAllContacts();
         return findElement(contactList).getText();
+    }
+
+    public void loadAllContacts(){
+            while(findElement(showMoreLink).isDisplayed()) {
+                findElement(showMoreLink).click();
+                waitForLoadingScreen();
+            }
+
     }
 
     public ContactDetailsPage viewContactDetails() {
