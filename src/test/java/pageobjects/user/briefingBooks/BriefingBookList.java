e7527b31b2391e5b5b6974de6665a09bdb90e31f
noelc <noelc@q4inc.com>
2017-01-11 16:35:56 -0500

    fixing selectors

--- src/test/java/pageobjects/user/briefingBooks/BriefingBookList.java
+++ src/test/java/pageobjects/user/briefingBooks/BriefingBookList.java
@@ -19,7 +19,7 @@ public class BriefingBookList extends AbstractPageObject {
     private final By createBookButton = By.cssSelector(".q4-hero-banner .action-button");
     private final By newBriefingBook = By.cssSelector(".briefing-book-item:nth-child(1)");
     private final By checkbox = By.className("checkmark");
-    private final By deleteButton = By.className("toolbar-button");
+    private final By deleteButton = By.className("q4i-trashbin-4pt");
     private final By confirmDeleteButton = By.className("x-button-action");
     private final By searchBox = By.cssSelector(".briefing-book-toolbar [type=search]");
     private final By briefingBookTitle = By.cssSelector(".briefing-book-item .row div:nth-child(2)");
@@ -55,7 +55,7 @@ public class BriefingBookList extends AbstractPageObject {
             strings.add(item.findElement(By.xpath(searchString)));
             System.out.print(item.findElement(By.xpath(searchString)).getText()+"\n");
         }
-        return elementsAreAlphaUpSorted(strings);
+        return elementsAreAlphaUpSortedIgnoreCase(strings);
     }
 
     private boolean isSortedByDate(BriefingBookColumnType type){
