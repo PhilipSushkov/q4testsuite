7a0edcd0b25bab2417d8ca0794e500814cf37684
PatrickPriestley <patrickp@q4websystems.com>
2017-01-11 10:29:28 -0500

    Adding more missing test classes to nightly runs

--- src/test/java/testrunner/NightRun.java
+++ src/test/java/testrunner/NightRun.java
@@ -13,6 +13,8 @@ import specs.user.dashboard.DashboardLogActivity;
 import specs.user.dashboard.DashboardSearch;
 import specs.user.login.ForgotPassword;
 import specs.user.login.UserLogin;
+import specs.user.reportBuilder.CreateBriefingBook;
+import specs.user.reportBuilder.EditBriefingBook;
 import specs.user.research.ResearchList;
 import specs.user.securityDetails.Overview;
 import specs.user.securityDetails.Ownership;
@@ -40,7 +42,9 @@ import specs.user.watchlist.EditWatchlist;
         SearchForActivity.class,
         TargetingList.class,
         ResearchList.class,
-        Ownership.class
+        Ownership.class,
+        CreateBriefingBook.class,
+        EditBriefingBook.class
 })
 
 public class NightRun {
