30db7925cb02b9c3947a51dd5ddca58f07d954c9
noelc <noelc@q4inc.com>
2017-01-16 15:36:13 -0500

    adding morning coffe report tests

--- src/test/java/pageobjects/admin/morningCoffeePage/MorningCoffeePage.java
+++ src/test/java/pageobjects/admin/morningCoffeePage/MorningCoffeePage.java
@@ -1,9 +1,18 @@
 package pageobjects.admin.morningCoffeePage;
 
+import org.joda.time.DateTime;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import pageobjects.AbstractPageObject;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by noelc on 2017-01-09.
@@ -18,6 +27,9 @@ public class MorningCoffeePage extends AbstractPageObject {
     private final By companySymbolTextField =By.xpath("//q4-morning-coffee-create//input");
     private final By reportCreationCancelButton = By.xpath("//q4-morning-coffee-create//button[contains(text(),'Cancel')]");
     private final By reportCreationCreateButton = By.xpath("//q4-morning-coffee-create//button[contains(text(),'Create')]");
+    private final By reportTableRows = By.xpath("//tr[contains(@class, 'ui-datatable')]");
+    private ArrayList<WebElement> tableRowsReports = new ArrayList<>();
+    private String username="QA Test";
 
     public MorningCoffeePage(WebDriver driver){super(driver);}
 
@@ -27,9 +39,11 @@ public class MorningCoffeePage extends AbstractPageObject {
         return this;
     }
 
-    public void inputCompanySymbol(String symbol){
+    public MorningCoffeePage inputCompanySymbol(String symbol){
         findElement(companySymbolTextField).click();
         findElement(companySymbolTextField).sendKeys(symbol);
+        findElement(By.xpath("//li[contains(@class,'ui-autocomplete-list-item') and .//div[contains(text(),'"+symbol+"')]]")).click(); //This is the autocomplete element. A report cannot be created until this is selected
+        return this;
     }
 
     public MorningCoffeePage clickCreateReport(){
@@ -42,6 +56,26 @@ public class MorningCoffeePage extends AbstractPageObject {
         findElement(reportCreationCancelButton).click();
         waitForLoadingScreen();
         return this;
+    }
+
+    private List<WebElement> readTableRows(){
+        waitForLoadingScreen();
+        List<WebElement> rowContents = findElements(reportTableRows);
+        return rowContents;
+    }
+
+    public boolean findRecentReport(String symbol, Date currentDate){
+        tableRowsReports = new ArrayList<>(readTableRows());
+        DateFormat dateFormat = new SimpleDateFormat("MMM DD, YYYY");
+        System.out.print(dateFormat.format(currentDate).toString());
+        if(tableRowsReports!=null){
+            for(WebElement row : tableRowsReports) {
+                if (row.getText().contains(symbol) && row.getText().contains(username) &&  row.getText().contains(dateFormat.format(currentDate).toString())){
+                    return true;
+                }
+            }
+        }
+    return false; //only gets here if the record is not found or the table is blank.
     }
 
 
---
+++ src/test/java/specs/admin/morningCoffee/morningCoffeeReport.java
@@ -0,0 +1,45 @@
+package specs.admin.morningCoffee;
+
+import org.joda.time.DateTime;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import pageobjects.admin.loginPage.AdminLoginPage;
+import pageobjects.admin.morningCoffeePage.MorningCoffeePage;
+import specs.AdminAbstractSpec;
+
+import java.util.Date;
+
+/**
+ * Created by noelc on 2017-01-16.
+ */
+public class morningCoffeeReport extends AdminAbstractSpec {
+
+
+    @Before
+    public void setUp() {
+        new AdminLoginPage(driver).loginAdmin()
+                .navigateToMorningCoffeePage();
+    }
+
+    @Test
+    public void canCreateReport(){
+        String symbol ="AAPL";
+        Date currentDate = new Date();
+       MorningCoffeePage morningCoffeePage =  new MorningCoffeePage(driver);
+       morningCoffeePage.clickAddReport().inputCompanySymbol(symbol).clickCreateReport();
+        Assert.assertTrue("Record not found",morningCoffeePage.findRecentReport(symbol,currentDate)); //current Date is used to check that is the report from today, no other way to identify a unique report.
+
+    }
+
+    @Test
+    public void canCancelAReportGeneration(){
+        String symbol ="NFLX";
+        Date currentDate = new Date();
+        MorningCoffeePage morningCoffeePage =  new MorningCoffeePage(driver);
+        morningCoffeePage.clickAddReport().inputCompanySymbol(symbol).clickCancelReport();
+        Assert.assertFalse("Record not found",morningCoffeePage.findRecentReport(symbol,currentDate));
+    }
+
+}
