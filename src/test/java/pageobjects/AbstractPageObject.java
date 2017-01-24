329fd4f71dea647d49e1c1d5f6fec6158ce691a0
noelc <noelc@q4inc.com>
2017-01-24 13:50:09 -0500

    increasing the waitdriver timeout to 10.

--- src/test/java/pageobjects/AbstractPageObject.java
+++ src/test/java/pageobjects/AbstractPageObject.java
@@ -60,7 +60,7 @@ public class AbstractPageObject implements PageObject {
 
     public AbstractPageObject(WebDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, 5L);
+        this.wait = new WebDriverWait(driver, 10L);
     }
 
     @Override
