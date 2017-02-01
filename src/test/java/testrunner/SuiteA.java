53aa8f80bb9eb3cbcb1d370b47e4158ff10619dc
PatrickPriestley <patrickp@q4websystems.com>
2017-02-01 09:02:58 -0500

    Adding tests that weren't run to SuiteA
    Trying to debug the timeout

--- src/test/java/testrunner/SuiteA.java
+++ src/test/java/testrunner/SuiteA.java
@@ -6,21 +6,11 @@ package testrunner;
  */
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import specs.user.activity.FilterActivity;
-import specs.user.activity.LogActivity;
 import specs.user.activity.SearchForActivity;
 import specs.user.contacts.ContactDetails;
-import specs.user.contacts.ContactList;
-import specs.user.dashboard.DashboardBuildReport;
-import specs.user.dashboard.DashboardLinks;
-import specs.user.dashboard.DashboardLogActivity;
-import specs.user.dashboard.DashboardSearch;
-import specs.user.login.ForgotPassword;
-import specs.user.login.UserLogin;
 import specs.user.reportBuilder.CreateBriefingBook;
 import specs.user.reportBuilder.EditBriefingBook;
 import specs.user.research.ResearchList;
-import specs.user.securityDetails.Overview;
 import specs.user.securityDetails.Ownership;
 import specs.user.sideNavBar.TabNavigationExpanded;
 import specs.user.targeting.TargetingList;
@@ -30,7 +20,16 @@ import specs.user.watchlist.EditWatchlist;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
 
-        Ownership.class
+        ContactDetails.class,
+        TabNavigationExpanded.class,
+        EditWatchlist.class,
+        Q4TeamPage.class,
+        SearchForActivity.class,
+        TargetingList.class,
+        ResearchList.class,
+        Ownership.class,
+        CreateBriefingBook.class,
+        EditBriefingBook.class
 })
 
 
