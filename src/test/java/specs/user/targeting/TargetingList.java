104304caeb83dafc79518ed56acd1a5979111355
noelc <noelc@q4inc.com>
2017-03-01 10:14:33 -0500

    targeting test fixes

--- src/test/java/specs/user/targeting/TargetingList.java
+++ src/test/java/specs/user/targeting/TargetingList.java
@@ -233,8 +233,6 @@ public class TargetingList extends AbstractSpec {
        targetingPage.deleteSearch(search);
    }
 
-   // TODO new changes have broken this flow. Ignoring so the rest of the suite runs
-   @Ignore
    @Test
    public void canAbortSearchDelete(){
        WebElement search;
@@ -248,8 +246,6 @@ public class TargetingList extends AbstractSpec {
        targetingPage.deleteSearch(search);
    }
 
-   // TODO new changes have broken this flow. Ignoring so the rest of the suite runs
-   @Ignore
    @Test
    public void canDeleteSavedSearch(){
        WebElement search;
