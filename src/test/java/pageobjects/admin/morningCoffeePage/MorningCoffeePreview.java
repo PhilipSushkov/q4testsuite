b656f3fa358104ab982734dc1412cd21ae020b8c
noelc <noelc@q4inc.com>
2017-03-22 14:14:53 -0400

    added a test that verifies that only tickers in the system can be added as companies.

--- src/test/java/pageobjects/admin/morningCoffeePage/MorningCoffeePreview.java
+++ src/test/java/pageobjects/admin/morningCoffeePage/MorningCoffeePreview.java
@@ -20,6 +20,17 @@ public class MorningCoffeePreview extends AbstractPageObject{
    private final By marketCommentaryText = By.xpath("//div[contains(@class,'col-1') and .//span[contains(text(),'Market Commentary')]]//div[contains(@class,'ql-editor')]");
     private final By sectorCommentaryText = By.xpath("//div[contains(@class,'col-1') and .//span[contains(text(),'Sector Commentary')]]//div[contains(@class,'ql-editor')]");
     private final By companyCommentaryText = By.xpath("//div[contains(@class,'col-1') and .//span[contains(text(),'Company Commentary')]]//div[contains(@class,'ql-editor')]");
+    private final By morningCoffeeTitle = By.xpath("//q4-morning-coffee-details//div[contains(text(),'Morning Coffee Report')]");
+
+    //Ugly selectors below. No real unique styling for this group of elements.
+    //Stock summary selectors
+    private final By stockSummary = By.xpath("//h3[contains(text(),'Stock Summary')]");
+    private final By stockSummaryStockTitle = By.xpath("//q4-table-stock-summary/p-datatable//span[contains(text(),'Stock ($)')]");
+    private final By stockSummaryChangeTitle= By.xpath("//q4-table-stock-summary/p-datatable//span[contains(text(),'Change ($)')]");
+    private final By stockSummaryVolumeTitle = By.xpath("//q4-table-stock-summary/p-datatable//span[contains(text(),'volume')]");
+    private final By stockSummaryStockAmount = By.xpath("//q4-table-stock-summary/p-datatable//table/tbody/tr/td[1]");
+    private final By stockSummaryChangeAmount =By.xpath("//q4-table-stock-summary/p-datatable//table/tbody/tr/td[2]");
+    private final By stockSummaryVolumeAmount =By.xpath("//q4-table-stock-summary/p-datatable//table/tbody/tr/td[3]");
 
 
     public MorningCoffeePreview(WebDriver driver){
@@ -54,6 +65,15 @@ public class MorningCoffeePreview extends AbstractPageObject{
         }
     }
 
+    public boolean previewPageLoaded(){
+        try {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(morningCoffeeTitle));
+            return true;
+        }
+        catch(Exception e){
+            return false;
+        }
+    }
 
     public boolean canCancelDelete(){
         clickDeleteIcon();
@@ -82,5 +102,15 @@ public class MorningCoffeePreview extends AbstractPageObject{
         return new MorningCoffeePage(driver);
     }
 
-
+    public boolean stockSummaryPresent() {
+        try {
+            wait.until(ExpectedConditions.visibilityOf(findElement(stockSummary)));
+            wait.until(ExpectedConditions.visibilityOf(findElement(stockSummaryStockTitle)));
+            wait.until(ExpectedConditions.visibilityOf(findElement(stockSummaryChangeTitle)));
+            wait.until(ExpectedConditions.visibilityOf(findElement(stockSummaryVolumeTitle)));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
--- src/test/java/specs/admin/morningCoffee/morningCoffeeReport.java
+++ src/test/java/specs/admin/morningCoffee/morningCoffeeReport.java
@@ -51,7 +51,14 @@ public class morningCoffeeReport extends AdminAbstractSpec {
         String symbol ="YUM";
         Date currentDate = new Date();
         MorningCoffeePage morningCoffeePage =  new MorningCoffeePage(driver);
-        morningCoffeePage.clickAddReport().inputCompanySymbol(symbol).clickCreateReport().clickRecentReport(symbol, currentDate);
+        Assert.assertTrue("Preview of report didn't load",morningCoffeePage.clickAddReport().inputCompanySymbol(symbol).clickCreateReport().clickRecentReport(symbol, currentDate).previewPageLoaded());
+    }
+    @Test
+    public void stockSummaryPresentInReportPreview(){
+        String symbol ="SYY";
+        Date currentDate = new Date();
+        MorningCoffeePage morningCoffeePage =  new MorningCoffeePage(driver);
+       Assert.assertTrue("Stock summary not present",morningCoffeePage.clickAddReport().inputCompanySymbol(symbol).clickCreateReport().clickRecentReport(symbol, currentDate).stockSummaryPresent());
     }
 
     @Test
