feac5e62701eb39c408ee4dc2acbf12e23a190bb
Andy Park <hg4park@uwaterloo.ca>
2017-06-06 12:02:58 -0400

    Added another suite

--- src/test/java/testrunner/andysuite.java
+++ src/test/java/testrunner/andysuite.java
@@ -3,12 +3,14 @@ package testrunner;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import specs.user.advancedSearch.AdvancedSearching;
+import specs.user.briefingBooks.CreateBriefingBook;
 
 /**
  * Created by andyp on 2017-06-06.
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
+        CreateBriefingBook.class,
         AdvancedSearching.class
 })
 public class andysuite {
