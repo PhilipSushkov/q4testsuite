c6e7a94bc43681267cd6e9f09019c8d28326c8aa
Andy Park <hg4park@uwaterloo.ca>
2017-06-23 14:33:12 -0400

    Webcast test (#130)
    * Test for adding a tag
    
    Added test to see if
    1: Tag can be/ has been added
    2. Same tag shows up when viewed from activity page
    
    * Bye bye redundant imports
    
    * Search for activity title before selecting activity
    
    Noticed a test failed because tests would always select first item on
    the activity list (which doesn’t always work). New solution: search for
    the activity first, then select it for tests.
    
    * Checks if location is correct
    
    Checks if location displayed on the activity page is the same as one
    displayed inside the details page.
    
    * Example for zach
    
    * Webcast Analytics
    
    * More changes
    
    * Making sure tests run smoothly
    
    * Fixed stuff so that there's no more errors
    
    Woo
    
    * WebAnalytics page check

--- src/test/java/specs/user/WebAnalytics/WebAnalytics.java
+++ src/test/java/specs/user/WebAnalytics/WebAnalytics.java
@@ -1,9 +1,42 @@
 package specs.user.WebAnalytics;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.testng.Assert;
+import pageobjects.user.loginPage.LoginPage;
+import pageobjects.user.webAnalyticsPage.WebAnalyticsPage;
+import specs.AbstractSpec;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by kelvint on 11/1/16.
  */
-public class WebAnalytics {
+public class WebAnalytics extends AbstractSpec {
+
+    @Before
+    public void setUp(){
+        new LoginPage(driver).loginUser()
+                .accessSideNav()
+                .selectWebAnalyticsFromSideNav();
+    }
+
+    @Test
+    public void checkCustomDateRange() {
+        //Sets custom date to today's date for both start and end dates, then compare
+        DateFormat dateFormat = new SimpleDateFormat("MMMM dd, yyyy");
+        String date = String.valueOf(dateFormat.format(new Date()));
+        String sDate = date + " - " + date;
+
+        WebAnalyticsPage page = new WebAnalyticsPage(driver);
+
+        page.selectCustomBtn();
+        page.updateCustomDateRange();
+
+        Assert.assertEquals(page.getDateRange(), sDate, "Date range does not match expected date range");
+    }
 
 
 
