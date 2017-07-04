9345a8f9042b8a072f6b3bbe0bd8be05d1611ff2
Andy Park <hg4park@uwaterloo.ca>
2017-07-04 16:12:45 -0400

    Added test suite

---
+++ src/test/java/testrunner/TestSuite.java
@@ -0,0 +1,19 @@
+package testrunner;
+
+/**
+ * Created by andyp on 2017-07-04.
+ */
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import specs.user.activity.ActivityDetails;
+import specs.user.activity.LogActivity;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        ActivityDetails.class,
+        LogActivity.class
+})
+public class TestSuite {
+
+}
