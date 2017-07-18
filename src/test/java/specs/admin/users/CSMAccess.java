0ddcefd57311e9fa889dce593b9e14033f1136ce
noelc <noelc@q4inc.com>
2017-07-18 15:29:10 -0400

    CSM access

--- src/test/java/specs/admin/users/CSMAccess.java
+++ src/test/java/specs/admin/users/CSMAccess.java
@@ -1,14 +1,20 @@
 package specs.admin.users;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import pageobjects.admin.companyPage.CompanyDetailsPage;
+import pageobjects.admin.companyPage.CompanyList;
 import pageobjects.admin.homePage.HomePage;
 import pageobjects.admin.loginPage.AdminLoginPage;
 import pageobjects.admin.usersPage.UserRole;
 import pageobjects.admin.usersPage.UserRoleDetails;
 import pageobjects.admin.usersPage.UsersPage;
 import specs.AdminAbstractSpec;
+
+import java.util.Date;
+
 /**
  * Created by noelc on 2017-07-18.
  */
@@ -25,13 +31,13 @@ public class CSMAccess extends AdminAbstractSpec {
     }
 
     @Test
-    public void userSelectTest (){
-
-
-
+    public void canEditCompanyProfile(){
+        String editedCompanyName = "Harmonic, Inc. ";
+        Date today = new Date();
+        CompanyList companyList = new CompanyList(driver).navigateToCompanyPage();
+        CompanyDetailsPage companyDetails = companyList.searchForCompany("Harmonic").selectFirstCompanyInList();
+        companyDetails.editCompanyName(editedCompanyName+ today);
+       Assert.assertTrue("Company name not edited",companyDetails.getCompanyName().contains(editedCompanyName+today));
     }
-    @Test
-    public  void whatever2(){
 
-    }
 }
