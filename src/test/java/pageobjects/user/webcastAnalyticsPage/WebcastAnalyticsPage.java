b87d3be25aa47203dc4dff97c8c17c198fba0107
Danny Liu <danny98liu@gmail.com>
2017-08-10 13:43:27 -0400

    Webcast Analytics sort test

--- src/test/java/pageobjects/user/webcastAnalyticsPage/WebcastAnalyticsPage.java
+++ src/test/java/pageobjects/user/webcastAnalyticsPage/WebcastAnalyticsPage.java
@@ -2,7 +2,11 @@ package pageobjects.user.webcastAnalyticsPage;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import pageobjects.AbstractPageObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by patrickp on 2016-08-16.
@@ -17,9 +21,21 @@ public class WebcastAnalyticsPage extends AbstractPageObject {
     private final By firstWebcastOnTheListLiveAttendees = By.cssSelector("div > div:nth-child(3) > span.counter");
     private final By firstWebcastOnTheListOnDemandAttendees = By.cssSelector("div > div:nth-child(4) > span.counter");
     private final By firstWebcastOnTheListTotalAttendees = By.cssSelector("div > div:nth-child(5) > span.counter");
+    private final By detailsHeader = By.xpath("//div[contains(@class, 'x-unsized x-button x-button-no-icon column flex x-text-align-left')]");
+    private final By registrantsHeader = By.xpath("//div[contains(@class, 'x-unsized x-button x-button-no-icon column medium centered')][1]");
+    private final By liveAttendeesHeader = By.xpath("//div[contains(@class, 'x-unsized x-button x-button-no-icon column medium centered')][2]");
+    private final By onDemandAttendeesHeader = By.xpath("//div[contains(@class, 'x-unsized x-button x-button-no-icon column medium centered')][3]");
+    private final By totalAttendeesHeader = By.xpath("//div[contains(@class, 'x-unsized x-button x-button-no-icon column medium centered')][4]");
 
     public WebcastAnalyticsPage(WebDriver driver) {
         super(driver);
+    }
+
+    private List<WebElement> returnTableRows (){
+        List<WebElement> rowList = findVisibleElements(By.xpath("//div[contains(@class, 'x-dataview-item webcast-item')]"));
+        ArrayList<WebElement> tableRowsList = new ArrayList<>(rowList);
+        return tableRowsList;
+
     }
 
     public WebcastAnalyticsDetailsPage selectFirstWebcast(){
@@ -53,5 +69,91 @@ public class WebcastAnalyticsPage extends AbstractPageObject {
         waitForLoadingScreen();
         return findElement(firstWebcastOnTheListTotalAttendees).getText();
     }
+
+    public boolean isColumnAscending(By column){
+        return findElement(column).getAttribute("class").contains("asc");
+
+    }
+
+    public boolean canSortByDetails(){
+        waitForElementToBeClickable(detailsHeader).click();
+        ArrayList<WebElement> titles = new ArrayList<>();
+        List<WebElement> rows = returnTableRows();
+        for (WebElement i : rows){
+            titles.add(i.findElement(By.className("title")));
+        }
+        if (isColumnAscending(detailsHeader)){
+            return elementsAreAlphaUpSortedIgnoreCase(titles);
+        }
+        else
+        {
+            return elementsAreAlphaDownSortedIgnoreCase(titles);
+        }
+    }
+
+    public boolean canSortByRegistrants(){
+        waitForElementToBeClickable(registrantsHeader).click();
+        ArrayList<WebElement> registrants = new ArrayList<>();
+        List<WebElement> rows = returnTableRows();
+        for (WebElement i : rows){
+            registrants.add(i.findElement(By.xpath("//div[contains(@class, 'column medium centered')][1]/span[contains(@class, 'counter')]")));
+        }
+        if (isColumnAscending(registrantsHeader)){
+            return elementsAreNumUpSorted(registrants);
+        }
+        else
+        {
+            return elementsAreAlphaDownSortedIgnoreCase(registrants);
+        }
+    }
+
+    public boolean canSortByLiveAttendees(){
+        waitForElementToBeClickable(liveAttendeesHeader).click();
+        ArrayList<WebElement> liveAttendees = new ArrayList<>();
+        List<WebElement> rows = returnTableRows();
+        for (WebElement i : rows){
+            liveAttendees.add(i.findElement(By.xpath("//div[contains(@class, 'column medium centered')][2]/span[contains(@class, 'counter')]")));
+        }
+        if (isColumnAscending(liveAttendeesHeader)){
+            return elementsAreNumUpSorted(liveAttendees);
+        }
+        else
+        {
+            return elementsAreNumUpSorted(liveAttendees);
+        }
+    }
+
+    public boolean canSortByOnDemandAttendees(){
+        waitForElementToBeClickable(onDemandAttendeesHeader).click();
+        ArrayList<WebElement> onDemandAttendees = new ArrayList<>();
+        List<WebElement> rows = returnTableRows();
+        for (WebElement i : rows){
+            onDemandAttendees.add(i.findElement(By.xpath("//div[contains(@class, 'column medium centered')][3]/span[contains(@class, 'counter')]")));
+        }
+        if (isColumnAscending(onDemandAttendeesHeader)){
+            return elementsAreNumUpSorted(onDemandAttendees);
+        }
+        else
+        {
+            return elementsAreNumUpSorted(onDemandAttendees);
+        }
+    }
+
+    public boolean canSortByTotalAttendees(){
+        waitForElementToBeClickable(totalAttendeesHeader).click();
+        ArrayList<WebElement> totalAttendees = new ArrayList<>();
+        List<WebElement> rows = returnTableRows();
+        for (WebElement i : rows){
+            totalAttendees.add(i.findElement(By.xpath("//div[contains(@class, 'column medium centered')][4]/span[contains(@class, 'counter')]")));
+        }
+        if (isColumnAscending(totalAttendeesHeader)){
+            return elementsAreNumUpSorted(totalAttendees);
+        }
+        else
+        {
+            return elementsAreNumUpSorted(totalAttendees);
+        }
+    }
+
 
 }
