c49882c6d933a5d5e5ba2ef9e52ee97d609c23b8
noelc <noelc@q4inc.com>
2017-10-03 11:26:20 -0400

    header file selector

--- src/test/java/testrunner/NightRunC.java
+++ src/test/java/testrunner/NightRunC.java
@@ -13,8 +13,11 @@ import specs.user.dashboard.DashboardSearch;
 import specs.user.header.header;
 import specs.user.login.ForgotPassword;
 import specs.user.login.UserLogin;
+import specs.user.ownership.Ownership;
+import specs.user.research.ResearchList;
 import specs.user.securityDetails.Overview;
 import specs.user.sentiment.Sentiment;
+import specs.user.targeting.TargetingList;
 import specs.user.volatility.Volatility;
 import specs.user.webcastAnalytics.WebcastAnalytics;
 
@@ -38,7 +41,6 @@ import specs.user.webcastAnalytics.WebcastAnalytics;
         TargetingList.class,
         ResearchList.class,
         Ownership.class
-
 })
 public class NightRunC {
 }
